86b7fcebe52e902b0cacb38ec91373917e234a6f
Tim Barlow <timbarlow1091@gmail.com>
2025-10-19 20:18:37 +0200

    Path updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import Footer from './components/Footer';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Programs from './pages/Programs';
+import Packages from './pages/Packages';
+import Testimonials from './pages/Testimonials';
+import Contact from './pages/Contact';
+
 
 export default function App() {
   return (
@@ -12,6 +17,10 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/programs" element={<Programs />} />
+          <Route path="/packages" element={<Packages />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
       <Footer />
